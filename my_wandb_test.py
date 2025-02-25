eefe338d2534440199f5fea8259ab6370d66a8af
Advait858 <advait.advaitsharma@gmail.com>
2025-02-25 23:03:17 +0530

    Add W&B logs and script

---
+++ my_wandb_test.py
@@ -0,0 +1,13 @@
+import wandb
+
+# 1. Login to wandb (only once per session)
+wandb.login(key="002b9beb1304272a9f38f51e52d99718ff0db18a")
+
+# 2. Initialize your run
+wandb.init(project="distance_classification_project")
+
+# 3. Example usage: log a metric
+accuracy_value = 0.90
+wandb.log({"accuracy": accuracy_value})
+
+print("Done logging to W&B!")
